9f70860a63d14b2193fbccd33f21b5113eca4682
bainbain21 <urbain.maille@gmail.com>
2026-09-03 09:39:39 +0200

    TP00 informatique E2

---
+++ tp_lancement/call.py
@@ -0,0 +1,6 @@
+from test import hello
+
+hello("Jean")
+
+if __name__ == "__main__" :
+    print("I'm call.py")
---
+++ tp_lancement/test.py
@@ -0,0 +1,7 @@
+def hello(name: str) -> None :
+    print(f"Hello {name} ! ")
+
+print("Hello World ! ")
+print(f"Script name : {__name__}")
+if __name__ == "__main__" :
+    hello("Jeune Trublion")
