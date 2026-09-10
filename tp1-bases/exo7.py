82a21a8a8a07028c5d7e953be25c96628bf9f4ed
bainbain21 <urbain.maille@gmail.com>
2026-09-10 09:50:19 +0200

    ajout partiel exo7

---
+++ tp1-bases/exo7.py
@@ -0,0 +1,18 @@
+# générateur plaque d'immatriculation fr
+
+import random
+from random import randrange
+
+lettre = ["A","B","C","D","E","F","G","H","J","K","L","M","N","P","Q","R","S","T","V","W","X","Y","Z"]
+
+A = random.choice(lettre)
+B = random.choice(lettre)
+C = random.choice(lettre)
+
+I = randrange(10)
+II = randrange(10)
+III = randrange(10)
+
+D = random.choice(lettre)
+E = random.choice(lettre)
+F = random.choice(lettre)
