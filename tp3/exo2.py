7ed97f5cc9f2211d95fbedf42433ba42d351a4cc
bainbain21 <urbain.maille@gmail.com>
2026-09-24 08:40:38 +0200

    exo2.py

---
+++ tp3/exo2.py
@@ -0,0 +1,12 @@
+# fonction reccursive de Sycarrus
+
+def Syc(n):
+    if n <= 1 : return 1
+    if n%2 == 0 : 
+        res = Syc(n//2)
+        return res
+    elif n%2 != 0 : 
+        res = Syc(n*3+1)
+        return res
+
+print(Syc(30))
